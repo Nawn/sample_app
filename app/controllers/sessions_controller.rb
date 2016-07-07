87bae768d33513e182fd18a2c1ce674bb59c3194
Raul Gonzalez <Nawn1994@gmail.com>
2016-07-07 01:27:04 -0700

    Added login abilities

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user_info = session_credentials
+    user = User.find_by(email: user_info[:email].downcase)
+    if user && user.authenticate(user_info[:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    
+  end
+
+  private
+  def session_credentials
+    params.require(:session).permit(:email, :password)
+  end
 end
